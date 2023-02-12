746b9224755eac20127871583604900928cc0633
etcwoo <123440971+etcwoo@users.noreply.github.com>
2023-02-12 15:39:10 +0900

    Delete modules directory

--- modules/web-acl-associate/main.tf
+++
@@ -1,6 +0,0 @@
-resource "aws_wafv2_web_acl_association" "this" {
-  count = var.policy == null ? 0 : length(var.policy)
-
-  resource_arn = var.resource_arn[count.index]
-  web_acl_arn  = var.web_acl_arn[count.index]
-}
