9cecfb9e23229121444d506e848925594227e23b
etcwoo <woodonggyu92@gmail.com>
2023-03-01 16:44:58 +0900

    feat: Add module for logging configuration

---
+++ modules/logging-configuration/variables.tf
@@ -0,0 +1,27 @@
+variable "policy" {
+  description = "(Required) A logging configuration policy to create."
+  type        = list(any)
+  default     = null
+}
+
+variable "log_destination_configs" {
+  type        = list(string)
+  description = "(Required) The Amazon Kinesis Data Firehose, Cloudwatch Log log group, or S3 bucket Amazon Resource Names (ARNs) that you want to associate with the web ACL."
+}
+
+variable "resource_arn" {
+  type        = list(string)
+  description = "(Required) The Amazon Resource Name (ARN) of the web ACL that you want to associate with log_destination_configs."
+}
+
+variable "redacted_fields" {
+  type        = list(map(any))
+  description = "(Optional) The parts of the request that you want to keep out of the logs. Up to 100 redacted_fields blocks are supported."
+  default     = null
+}
+
+variable "logging_filter" {
+  type        = list(any)
+  description = "(Optional) A configuration block that specifies which web requests are kept in the logs and which are dropped. You can filter on the rule action and on the web request labels that were applied by matching rules during web ACL evaluation."
+  default     = null
+}
