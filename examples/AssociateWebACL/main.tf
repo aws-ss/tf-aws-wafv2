99173c72c2c206ace5f43e673a48bd6129aa14b3
etcwoo <woodonggyu92@gmail.com>
2023-03-04 16:36:27 +0900

    fix: terraform-fmt failed

--- examples/AssociateWebACL/main.tf
+++ examples/AssociateWebACL/main.tf
@@ -5,7 +5,7 @@ locals {
 module "wafv2" {
   source = "../..//modules/web-acl-associate"
 
-  policy            = local.config_vars.policy
-  resource_arn      = local.config_vars.policy[*].resource_arn
-  web_acl_arn       = local.config_vars.policy[*].web_acl_arn
+  policy       = local.config_vars.policy
+  resource_arn = local.config_vars.policy[*].resource_arn
+  web_acl_arn  = local.config_vars.policy[*].web_acl_arn
 }
